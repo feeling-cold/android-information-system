e83055f28c939f41977f9a1c0f3d5441556744ee
cool <2489321713@qq.com>
2021-08-24 00:03:04 +0800

    third

---
+++ app/src/main/java/com/example/myapplication1/MainActivity3.java
@@ -0,0 +1,20 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity3 extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
