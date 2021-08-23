f6b4976a95cc7ddf109b158f670dd8abdf6b9bd8
cool <2489321713@qq.com>
2021-08-23 18:25:18 +0800

    twice

--- app/src/main/java/com/example/gradesystem/MainActivity2.java
+++ app/src/main/java/com/example/gradesystem/MainActivity2.java
@@ -1,14 +1,14 @@
 package com.example.gradesystem;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class workspace extends AppCompatActivity {
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 }
